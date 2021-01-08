0f961f765300e098138fc25b97582d77e40d1d76
Imri Oz <imrioz@gmail.com>
2021-01-08 19:54:21 +0000

    first push

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web API DenyList design
+# fblx
---
+++ authSrv/demo-app-auth.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Demo APP server
+"""
+
+import uuid
+import falcon
+#import json
+#import logging
+
+class HandleRequest(object):
+    def on_post(self, req, resp):
+        token = req.get_header('X-Auth-Token',required=True)
+        if uuid.UUID(token).int % 2:
+            resp.status = falcon.HTTP_200
+        else:
+            resp.status = falcon.HTTP_200
+
+app = falcon.API()
+
+app.add_route('/check_status', HandleRequest())
+
+# Useful for debugging problems in API, it works with pdb
+if __name__ == '__main__':
+    from wsgiref import simple_server  # NOQA
+    httpd = simple_server.make_server('127.0.0.1', 7000, app)
+    httpd.serve_forever()
+
